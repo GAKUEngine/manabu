daf54cfca415a304d4801be7e609b941376a1839
Georgi Tapalilov <tapalilov@gmail.com>
2017-11-22 17:44:38 +0200

    run docker before specs and stop ot after that

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,21 @@
 require 'rspec'
 require 'manabu'
 # require 'gaku'
+require 'faraday'
 
 RSpec.configure do |config|
   config.before(:suite) do
+    system('cd ../gaku; bin/gaku container start')
+    loop do
+      res = Faraday.get('http://localhost:9000/api/status') rescue nil
+      break if res && res.status == 200
+      puts 'Waiting'
+      sleep 5
+    end
+
   end
 
   config.after(:suite) do
+    system('cd ../gaku; bin/gaku container delete')
   end
 end
