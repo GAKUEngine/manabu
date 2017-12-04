1af615710720746f3278bf0fbd1bd81b80505d23
zero <zero@phantom.industries>
2017-12-04 15:52:59 +0900

    optional external test container

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,17 +5,28 @@ require 'gaku/container'
 
 RSpec.configure do |config|
   config.before(:suite) do
-    Gaku::Container.Start
-    loop do
-      res = Faraday.get('http://localhost:9000/api/v1/status') rescue nil
-      break if res && res.status == 200
-      puts 'Waiting'
-      sleep 5
+    if check_test_container() # if running a test container externally, leave it
+      puts 'Detected running container, not intializing a new container'
+      $external_test_container = true
+    else
+      $external_test_container = false
+      Gaku::Container.Start
+      loop do
+        break if check_test_container()
+        puts 'Waiting for server instance'
+        sleep 5
+      end
     end
-
   end
 
   config.after(:suite) do
-    Gaku::Container.Delete
+    Gaku::Container.Delete unless $external_test_container
+  end
+
+  def check_test_container()
+    res = Faraday.get('http://localhost:9000/api/v1/status') rescue nil
+    return true if res && res.status == 200
+    false
   end
 end
+
